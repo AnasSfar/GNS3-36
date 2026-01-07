8bd0b4891328e55d7a564f1e03ff538e80cdfcba
AnasSfar <sfaranas13@gmail.com>
2026-01-07 09:42:39 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ import json
 import ipaddress
 from collections import defaultdict
 
+#lire l'intent file
+with open("intentfile.json") as f:
+    intent = json.load(f)
+ases = intent["ases"]
+routers_def = intent["routers"]
+links = intent["links"]
+
